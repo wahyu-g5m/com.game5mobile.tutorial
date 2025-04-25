e0f305d52cefb3ff249b901543710e568dd0a175
wahyugam5mobile <wahyu@game5mobile>
2025-04-25 07:43:23 +0700

    init: initialize package

---
+++ Runtime/YieldableTutorial.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+
+public abstract class YieldableTutorial : Tutorial
+{
+    private bool isDone;
+
+    protected abstract IEnumerator InitializeRoutine();
+    protected abstract IEnumerator RunRoutine();
+    public abstract IEnumerator DisposeRoutine();
+
+    public virtual void EndRun()
+    {
+        isDone = true;
+    }
+
+    public IEnumerator ExecuteRoutine()
+    {
+        yield return InitializeRoutine();
+
+        if (GetState() == State.Ready)
+        {
+            yield return RunRoutine();
+
+            while (!isDone)
+            {
+                yield return null;
+            }
+
+            yield return DisposeRoutine();
+        }
+    }
+}
